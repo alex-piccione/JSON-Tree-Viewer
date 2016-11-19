4f15df4f727acd4e2e72e9ac88bc2241c7e966ef
Alex75 <alessandro.piccione.75@gmail.com>
2016-11-19 21:22:28 +0000

    Start project

---
+++ Windows Form/JsonViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alex75.JsonViewer.Windows_Form
+{
+    public partial class JsonViewer : UserControl
+    {
+        public JsonViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Windows Form/TreeView.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Alex75.JsonViewer.Windows_Form
+{
+    partial class TreeView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tree = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tree
+            // 
+            this.tree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tree.Location = new System.Drawing.Point(0, 0);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(150, 150);
+            this.tree.TabIndex = 0;
+            // 
+            // TreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tree);
+            this.Name = "TreeView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tree;
+    }
+}
