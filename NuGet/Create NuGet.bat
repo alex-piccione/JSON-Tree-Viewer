fda7a1b539050cf60f4496c2f3deea0fc5c60f3b
Alex75 <alessandro.piccione.75@gmail.com>
2016-11-26 19:08:14 +0000

    NuGet batch command works also without cleaning folder. Previous created packages are stored in the "old" folder. Add date, integer and float values to the example JSON file.

--- NuGet/Create NuGet.bat
+++ NuGet/Create NuGet.bat
@@ -1,6 +1,12 @@
 
 @echo OFF
 rem http://docs.nuget.org/ndocs/tools/nuget.exe-cli-reference#pack
+rem Note. nuget.exe should exists in this directory or in PATH
+
+rem move previous packages in "old" directory
+if not exist "old" mkdir "old"
+move /y "*.nupkg" "old"
+
 
 nuget pack "..\JSON Viewer\JSON Viewer.csproj" -Prop Configuration=Release -build -IncludeReferencedProjects -symbols 
 rem this command create an EMPTY package !!!
--- NuGet/Publish NuGet package.bat
+++ NuGet/Publish NuGet package.bat
@@ -1,5 +1,6 @@
 @echo OFF
 rem http://docs.nuget.org/ndocs/tools/nuget.exe-cli-reference#push
+rem Note. nuget.exe should exists in this directory or in PATH
 
 rem nuget setApikey GUID -source https://www.nuget.org/api/v2/package
 nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
